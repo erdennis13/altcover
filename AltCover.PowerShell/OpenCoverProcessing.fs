76890d2aae71988ff3bef9f89d003c147363d051
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-06-13 10:36:23 +0100

    Work on post-processing OpenCover format output -- first placeholder

---
+++ AltCover.PowerShell/OpenCoverProcessing.fs
@@ -0,0 +1,64 @@
+﻿namespace AltCover.Commands
+
+#if MONO
+module OpenCoverProcessing =
+    let hello name =
+        printfn "Hello %s" name
+#else
+
+open System
+open System.Diagnostics.CodeAnalysis
+open System.Globalization
+open System.IO
+open System.Linq
+open System.Management.Automation
+open System.Xml
+open System.Xml.Linq
+open System.Xml.XPath
+
+open AltCover.PowerShell
+
+[<Cmdlet(VerbsCommon.Set, "CompilerGenerated")>]
+[<OutputType(typeof<XmlDocument>)>]
+[<SuppressMessage("Microsoft.PowerShell", "PS1008", Justification = "Cobertura is OK")>]
+type SetCompilerGeneratedCommand(outputFile:String) =
+  inherit PSCmdlet()
+
+  new () = SetCompilerGeneratedCommand(String.Empty)
+
+  [<SuppressMessage("Microsoft.Design", "CA1059", Justification="converts concrete types")>]
+  [<Parameter(ParameterSetName = "XmlDoc", Mandatory = true, Position = 1,
+      ValueFromPipeline = true, ValueFromPipelineByPropertyName = false)>]
+  member val XmlDocument:XmlDocument = null with get, set
+
+  [<Parameter(ParameterSetName = "FromFile", Mandatory = true, Position = 1,
+      ValueFromPipeline = true, ValueFromPipelineByPropertyName = false)>]
+  member val InputFile:string = null with get, set
+
+  [<Parameter(ParameterSetName = "XmlDoc", Mandatory = false, Position = 2,
+      ValueFromPipeline = false, ValueFromPipelineByPropertyName = false)>]
+  [<Parameter(ParameterSetName = "FromFile", Mandatory = false, Position = 2,
+      ValueFromPipeline = false, ValueFromPipelineByPropertyName = false)>]
+  member val OutputFile:string = outputFile with get, set
+
+  override self.ProcessRecord() =
+    let here = Directory.GetCurrentDirectory()
+    try
+      let where = self.SessionState.Path.CurrentLocation.Path
+      Directory.SetCurrentDirectory where
+      if self.ParameterSetName = "FromFile" then
+        self.XmlDocument <- XmlDocument()
+        self.XmlDocument.Load self.InputFile
+
+      let rewrite = self.XmlDocument
+      if self.OutputFile |> String.IsNullOrWhiteSpace |> not then
+        rewrite.Save(self.OutputFile)
+
+      self.WriteObject rewrite
+    finally
+      Directory.SetCurrentDirectory here
+
+
+#endif
+
+
