5b7ae2eb9e2f63b60c0bb600690f207e5e3eb14d
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-06-13 10:45:56 +0100

    And another placeholder

--- AltCover.PowerShell/OpenCoverProcessing.fs
+++ AltCover.PowerShell/OpenCoverProcessing.fs
@@ -58,7 +58,44 @@ type SetCompilerGeneratedCommand(outputFile:String) =
     finally
       Directory.SetCurrentDirectory here
 
+[<Cmdlet(VerbsData.Merge, "Coverage")>]
+[<OutputType(typeof<XmlDocument>)>]
+[<SuppressMessage("Microsoft.PowerShell", "PS1008", Justification = "Cobertura is OK")>]
+type MergeCoverageCommand(outputFile:String) =
+  inherit PSCmdlet()
+
+  new () = MergeCoverageCommand(String.Empty)
+
+  [<SuppressMessage("Microsoft.Design", "CA1059", Justification="converts concrete types")>]
+  [<Parameter(ParameterSetName = "XmlDoc", Mandatory = true, Position = 1,
+      ValueFromPipeline = true, ValueFromPipelineByPropertyName = false)>]
+  member val XmlDocument:XmlDocument = null with get, set
+
+  [<Parameter(ParameterSetName = "FromFile", Mandatory = true, Position = 1,
+      ValueFromPipeline = true, ValueFromPipelineByPropertyName = false)>]
+  member val InputFile:string = null with get, set
+
+  [<Parameter(ParameterSetName = "XmlDoc", Mandatory = false, Position = 2,
+      ValueFromPipeline = false, ValueFromPipelineByPropertyName = false)>]
+  [<Parameter(ParameterSetName = "FromFile", Mandatory = false, Position = 2,
+      ValueFromPipeline = false, ValueFromPipelineByPropertyName = false)>]
+  member val OutputFile:string = outputFile with get, set
+
+  override self.ProcessRecord() =
+    let here = Directory.GetCurrentDirectory()
+    try
+      let where = self.SessionState.Path.CurrentLocation.Path
+      Directory.SetCurrentDirectory where
+      if self.ParameterSetName = "FromFile" then
+        self.XmlDocument <- XmlDocument()
+        self.XmlDocument.Load self.InputFile
+
+      let rewrite = self.XmlDocument
+      if self.OutputFile |> String.IsNullOrWhiteSpace |> not then
+        rewrite.Save(self.OutputFile)
+
+      self.WriteObject rewrite
+    finally
+      Directory.SetCurrentDirectory here
 
 #endif
-
-
